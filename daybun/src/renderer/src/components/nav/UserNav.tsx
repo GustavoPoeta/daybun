a0c75b5bad488147c8e55ba5564bdac4a528dfa4
Gustavo Poeta <gustavpoeta@gmail.com>
2025-01-11 15:07:14 -0300

    🔧 chore: remove unused console.log in UserNav.tsx

--- daybun/src/renderer/src/components/nav/UserNav.tsx
+++ daybun/src/renderer/src/components/nav/UserNav.tsx
@@ -6,8 +6,6 @@ interface Props {
 
 export default function UserNav ({username, imagePath, navWidth} : Props): JSX.Element {
 
-    console.log(navWidth)
-
     return (
         <div className={`w-5/6 h-[10%] flex items-center ${navWidth <= 125 ? 'justify-center' : null} md:px-4 sm:px-2 px-0 md:rounded-lg sm:rounded-sm rounded-none border-b-2 mb-4 hover:bg-slate-200 hover:cursor-pointer`}>
             <img src={imagePath} alt={`${username}'s photo`} className="min-w-8 w-1/4 max-w-14 h-4/4 rounded-full"/>
