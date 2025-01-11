f24e807d58cd81a0e33532142505c0ca0b527546
Gustavo Poeta <gustavpoeta@gmail.com>
2025-01-11 12:34:00 -0300

    🚀 feat: create empty resizeNavBar function, wrap navigation and options and search in a div and add a invisible line to handle resizing

--- daybun/src/renderer/src/components/nav/Nav.tsx
+++ daybun/src/renderer/src/components/nav/Nav.tsx
@@ -4,16 +4,18 @@ import userImage2 from "../../assets/bunnyGirl.png";
 import userImage3 from "../../assets/image.png";
 import SearchUser from "./SearchUser";
 import optionSvg from "../../assets/gear-fill.svg";
-import searchSvg from "../../assets/search.svg";
+export default function Nav(): JSX.Element {
 
-export default function Nav (): JSX.Element {
+    const resizeNavBar = () => {
+
+    };
 
     return (
-        <>
-            <nav className="w-1/5 h-screen rounded-lg bg-gradient-to-b from-yellow-200 to-pink-300 shadow-[5px_0px_12px_0px_rgba(0,_0,_0,_0.2)]">
-                
-                {/* navigation between users*/}
-                <div className="w-[99%] h-[90%] pt-8 flex flex-col items-center bg-white rounded-tr-xl">
+        <nav className="w-1/5 h-screen flex rounded-lg bg-gradient-to-b from-yellow-200 to-pink-300 shadow-[5px_0px_12px_0px_rgba(0,_0,_0,_0.2)]">
+
+            <div className="w-[99%] h-full">
+                    {/* navigation between users*/}
+                <div className="w-full h-[90%] pt-8 flex flex-col items-center bg-white rounded-tr-xl">
                     <SearchUser />
                     <UserNav username="Ash" imagePath={userImage1} />
                     <UserNav username="Nic" imagePath={userImage2} />
@@ -21,13 +23,13 @@ export default function Nav (): JSX.Element {
                 </div>
 
                 {/* options and search */}
-                <div className="w-[99%] h-[10%] flex items-center justify-evenly  bg-white rounded-br-xl border-t-2 border-pink-100">
-                    <img src={searchSvg} alt="Search for users button" className="w-1/6 h-3/6 hover:cursor-pointer" />
-                    <img src={optionSvg} alt="Go to Options button" className="w-1/6 h-3/6 hover:cursor-pointer"/>
+                <div className="w-full h-[10%] flex items-center justify-evenly  bg-white rounded-br-xl border-t-2 border-pink-100">
+                    <img src={optionSvg} alt="Go to Options button" className="w-1/6 h-2/6 hover:cursor-pointer" />
                 </div>
-                
-            </nav>
-        </>
-        
+            </div>
+
+            <div className="w-[1%] h-full hover:cursor-e-resize"></div>
+
+        </nav>
     );
 };
